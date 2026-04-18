0ffa531ed644b9b724fb4a744c1642aeb7a3d5b0
nikroshupkin2002-maker <nik.roshupkin.2002@gmail.com>
2026-04-18 18:15:08 +0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,1 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { CalculatorPage } from "./pages/CalculatorPage";
+import { ProductsPage } from "./pages/ProductsPage";
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/calculator" element={<CalculatorPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        {/* В будущем добавишь сюда детальную страницу продукта */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
