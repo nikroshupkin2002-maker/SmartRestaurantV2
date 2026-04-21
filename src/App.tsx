51c332458f5fa1643863a95521b0dc6b17cd7c7c
nikroshupkin2002-maker <nik.roshupkin.2002@gmail.com>
2026-04-21 23:23:54 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 // @ts-nocheck
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { HomePage } from './pages/HomePage'; // Проверь название этого файла!
+import { HomePage } from './pages/HomePage';
 import { ProductsPage } from './pages/ProductsPage';
 import { ProductDetailPage } from './pages/ProductDetailPage';
+// Импортируй файл, где лежит сам калькулятор (проверь название файла!)
+import { CalculatorPage } from './pages/CalculatorPage'; 
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        {/* Теперь на главной будет Калькулятор (HomePage) */}
-        <Route path="/" element={<HomePage />} /> 
-        
-        {/* Список всех продуктов будет тут */}
+        <Route path="/" element={<HomePage />} />
+        {/* ВОТ ЭТА СТРОЧКА ИСПРАВИТ БЕЛЫЙ ЭКРАН: */}
+        <Route path="/calculator" element={<CalculatorPage />} /> 
         <Route path="/products" element={<ProductsPage />} />
-        
-        {/* Детальная страница продукта */}
         <Route path="/products/:id" element={<ProductDetailPage />} />
       </Routes>
     </Router>
