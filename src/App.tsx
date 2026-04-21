7371da971c164fc0677c020c30ab82eb90c83403
nikroshupkin2002-maker <nik.roshupkin.2002@gmail.com>
2026-04-21 22:46:33 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
-// ... остальные импорты
-import { ProductsPage } from "./pages/ProductsPage";
-import { ProductDetailPage } from "./pages/ProductDetailPage";
+// @ts-nocheck
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ProductsPage } from './pages/ProductsPage';
+import { ProductDetailPage } from './pages/ProductDetailPage';
+
+// Здесь можно импортировать главную страницу, если она у тебя есть
+// Например: import { HomePage } from './pages/HomePage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/calculator" element={<CalculatorPage />} />
-        
-        {/* Должно быть вот так: */}
+        {/* Если у тебя есть главная страница, замени ProductsPage ниже на HomePage */}
+        <Route path="/" element={<ProductsPage />} />
         <Route path="/products" element={<ProductsPage />} />
         <Route path="/products/:id" element={<ProductDetailPage />} />
       </Routes>
     </Router>
   );
 }
+
+// ВОТ ЭТА СТРОЧКА САМАЯ ВАЖНАЯ:
+export default App;
