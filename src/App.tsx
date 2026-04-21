5fbc5b117784d804c3f7139ba8fe5062cc8a9ac6
nikroshupkin2002-maker <nik.roshupkin.2002@gmail.com>
2026-04-21 21:48:12 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { HomePage } from "./pages/HomePage";
-import { CalculatorPage } from "./pages/CalculatorPage";
+// ... остальные импорты
 import { ProductsPage } from "./pages/ProductsPage";
+import { ProductDetailPage } from "./pages/ProductDetailPage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Главная */}
         <Route path="/" element={<HomePage />} />
-        
-        {/* Калькулятор */}
         <Route path="/calculator" element={<CalculatorPage />} />
         
-        {/* Продукты */}
+        {/* Должно быть вот так: */}
         <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<ProductDetailPage />} />
       </Routes>
     </Router>
   );
 }
-
-export default App;
