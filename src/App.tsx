9759e004191e966026648e08065d59ce1d654f61
nikroshupkin2002-maker <nik.roshupkin.2002@gmail.com>
2026-04-25 00:16:43 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-// Импортируем компоненты страниц
-// Важно: путь должен точно совпадать с папкой, где лежит файл
-import CalculatorPage from './pages/CalculatorPage'; 
-
-// Если у тебя есть главная страница, импортируй её тоже
-// import HomePage from './pages/HomePage'; 
+import HomePage from './pages/HomePage';
+import CalculatorPage from './pages/CalculatorPage';
 
 function App() {
   return (
     <Router>
-      <div className="app-container">
-        <Routes>
-          {/* Главная страница (если она есть, если нет - можно поставить калькулятор первой) */}
-          <Route path="/" element={<CalculatorPage />} />
-          
-          {/* Отдельный путь для калькулятора */}
-          <Route path="/calculator" element={<CalculatorPage />} />
-          
-          {/* Сюда можно добавить другие страницы в будущем */}
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/calculator" element={<CalculatorPage />} />
+      </Routes>
     </Router>
   );
 }
