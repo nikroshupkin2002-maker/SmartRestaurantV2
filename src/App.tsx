342a935c1acc78082c8bc14042468bfe6b64ad1a
nikroshupkin2002-maker <nik.roshupkin.2002@gmail.com>
2026-04-25 19:17:03 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
+// @ts-nocheck
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+// Импорт твоих страниц
 import { HomePage } from './pages/HomePage';
 import { CalculatorPage } from './pages/CalculatorPage';
 import { ProductsPage } from './pages/ProductsPage';
@@ -7,12 +11,26 @@ import { ProductDetailPage } from './pages/ProductDetailPage';
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/calculator" element={<CalculatorPage />} />
-        <Route path="/products" element={<ProductsPage />} />
-        <Route path="/products/:id" element={<ProductDetailPage />} />
-      </Routes>
+      <div className="min-h-screen bg-[#F4F7F9]">
+        <Routes>
+          {/* 1. Главная страница (Лендинг) */}
+          <Route path="/" element={<HomePage />} />
+
+          {/* 2. Страница калькулятора прибыли */}
+          <Route path="/calculator" element={<CalculatorPage />} />
+
+          {/* 3. Общий список продуктов (Выбор для КП и печать) */}
+          <Route path="/products" element={<ProductsPage />} />
+
+          {/* 4. Детальная страница конкретного продукта 
+              Важно: путь начинается с /products/, чтобы совпадать с Link из ProductsPage 
+          */}
+          <Route path="/products/:productId" element={<ProductDetailPage />} />
+
+          {/* Редирект на главную для любых неопознанных путей */}
+          <Route path="*" element={<HomePage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
