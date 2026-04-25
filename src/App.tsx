afb4a9721d97465eb71a1caac9acb8273462f969
nikroshupkin2002-maker <nik.roshupkin.2002@gmail.com>
2026-04-25 17:08:46 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import CalculatorPage from './pages/CalculatorPage';
+import { HomePage } from './pages/HomePage';
+import { CalculatorPage } from './pages/CalculatorPage';
+import { ProductsPage } from './pages/ProductsPage';
+import { ProductDetailPage } from './pages/ProductDetailPage';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/calculator" element={<CalculatorPage />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<ProductDetailPage />} />
       </Routes>
     </Router>
   );
